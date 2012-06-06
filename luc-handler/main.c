68ffde40eeeefa8d02da9cfa0a0f0d581fd1dc66
Francisco Marchena <francisco.marchena@codethink.co.uk>
2012-06-06 17:18:55 +0100

    Add luc-handler-dbus.xml and basic main.c

---
+++ luc-handler/main.c
@@ -0,0 +1,20 @@
+/* vi:set et ai sw=2 sts=2 ts=2: */
+/* -
+ * Copyright (c) 2012 GENIVI.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <glib.h>
+
+
+
+int
+main (int    argc,
+      char **argv)
+{
+  g_debug ("Hello");
+  return 0;
+}
