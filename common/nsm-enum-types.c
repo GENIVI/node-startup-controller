0397f87cdadfd55e4f5ed5b7dfd9da2f5415a5ac
Francisco Redondo Marchena <francisco.marchena@codethink.co.uk>
2012-07-23 16:18:43 +0100

    Define enums for shutdown mode, error and status code.
    Define enums for shutdown mode, error and status code.

---
+++ common/nsm-enum-types.c
@@ -0,0 +1,68 @@
+/* vi:set et ai sw=2 sts=2 ts=2: */
+/* -
+ * Copyright (c) 2012 GENIVI.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <glib/gi18n.h>
+#include <glib-object.h>
+
+#include <common/nsm-enum-types.h>
+
+
+
+GType
+nsm_shutdown_type_get_type (void)
+{
+  GType type = G_TYPE_INVALID;
+
+  if (G_UNLIKELY (type == G_TYPE_INVALID))
+    {
+      static const GEnumValue vaules[] =
+      {
+        { NSM_SHUTDOWN_TYPE_NOT,    "NSM_SHUTDOWN_TYPE_NOT",    N_ ("No shutdown"),        },
+        { NSM_SHUTDOWN_TYPE_NORMAL, "NSM_SHUTDOWN_TYPE_NORMAL", N_ ("Normal shutdown"),    },
+        { NSM_SHUTDOWN_TYPE_FAST,   "NSM_SHUTDOWN_TYPE_FAST",   N_ ("Fast shutdown"),      },
+        { NSM_SHUTDOWN_TYPE_RUNUP,  "NSM_SHUTDOWN_TYPE_RUNUP",  N_ ("Shutdown cancelled"), },
+        { 0 ,                       NULL,                       NULL,                      },
+      };
+
+      type = g_enum_register_static ("NSMShutdownType", values); 
+    }
+  return type;
+}
+
+
+
+GType
+nsm_error_status_get_type (void)
+{
+  GType type = G_TYPE_INVALID;
+
+  if (G_UNLIKELY (type == G_TYPE_INVALID))
+    {
+      static const GEnumValue vaules[] =
+      {
+        { NSM_ERROR_STATUS_NOT_SET,          "NSM_ERROR_STATUS_NOT_SET",          N_ ("Error not set"),           },
+        { NSM_ERROR_STATUS_OK,               "NSM_ERROR_STATUS_OK",               N_ ("No error"),                },
+        { NSM_ERROR_STATUS_ERROR,            "NSM_ERROR_STATUS_ERROR",            N_ ("Non-specific error"),      },
+        { NSM_ERROR_STATUS_DBUS,             "NSM_ERROR_STATUS_DBUS",             N_ ("Dbus comunication error"), },
+        { NSM_ERROR_STATUS_INTERNAL,         "NSM_ERROR_STATUS_INTERNAL",         N_ ("Internal error"),          },
+        { NSM_ERROR_STATUS_PARAMETER,        "NSM_ERROR_STATUS_PARAMETER",        N_ ("Parameter wrong"),         },
+        { NSM_ERROR_STATUS_WRONG_SESSION,    "NSM_ERROR_STATUS_WRONG_SESSION",    N_ ("Unknown session"),         },
+        { NSM_ERROR_STATUS_RESPONSE_PENDING, "NSM_ERROR_STATUS_RESPONSE_PENDING", N_ ("Reponse pending"),         },
+        { NSM_ERROR_STATUS_LAST,             "NSM_ERROR_STATUS_LAST",             N_ ("Last error"),              },
+        { 0 ,                                NULL,                                NULL,                           },
+      };
+
+      type = g_enum_register_static ("NSMErrorStatus", values);
+    }
+  return type;
+}
---
+++ common/nsm-enum-types.h
@@ -0,0 +1,52 @@
+/* vi:set et ai sw=2 sts=2 ts=2: */
+/* -
+ * Copyright (c) 2012 GENIVI.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef __NSM_ENUM_TYPES_H__
+#define __NSM_ENUM_TYPES_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define NSM_SHUTDOWN_TYPE (nsm_shutdown_type_get_type ())
+
+typedef enum /*< enum >*/
+{
+  NSM_SHUTDOWN_TYPE_NOT    = 0x00000000U, /* Client not registered for any shutdown */
+  NSM_SHUTDOWN_TYPE_NORMAL = 0x00000001U, /* Client registered for normal shutdown  */
+  NSM_SHUTDOWN_TYPE_FAST   = 0x00000002U, /* Client registered for fast shutdown    */
+  NSM_SHUTDOWN_TYPE_RUNUP  = 0x80000000U, /* The shutdown type "run up" can not be used
+                                             for registration. Clients which are
+                                             registered and have been shut down, will
+                                             automatically be informed about the "runup",
+                                             when the shut down is cancelled*/
+} NSMShutdownType;
+
+GType nsm_shutdown_type_get_type (void);
+
+#define NSM_ERROR_STATUS (nsm_error_status_get_type ())
+
+typedef enum /*< enum >*/
+{
+  NSM_ERROR_STATUS_NOT_SET,          /* Initial value when error type is not set       */
+  NSM_ERROR_STATUS_OK,               /* Value when no error occurred                   */
+  NSM_ERROR_STATUS_ERROR,            /* A general, non-specific error occurred         */
+  NSM_ERROR_STATUS_DBUS,             /* Error in D-Bus communication                   */
+  NSM_ERROR_STATUS_INTERNAL,         /* Internal error (memory alloc. failed, etc.)    */
+  NSM_ERROR_STATUS_PARAMETER,        /* A passed parameter was incorrect               */
+  NSM_ERROR_STATUS_WRONG_SESSION,    /* The requested session is unknown.              */
+  NSM_ERROR_STATUS_RESPONSE_PENDING, /* Command accepted, return value delivered asynch. */
+  NSM_ERROR_STATUS_LAST,             /* Last error value to identify valid errors.     */
+} NSMErrorStatus;
+
+GType nsm_error_status_get_type (void);
+
+G_END_DECLS
+
+#endif /* !__NSM_ENUM_TYPES_H__ */
