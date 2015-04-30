43ebb07aced530b178645d75ed9dd6c85324267e
Jonathan Maw <jonathan.maw@codethink.co.uk>
2015-04-30 10:57:56 +0100

    Change docs license to CC-BY-SA 4.0 and update licensing to GENIVI standard
    The previous docs format, CC 1.0, is not recommended by GENIVI, and
    the preferred one is CC-BY-SA 4.0.
    
    Other license changes are:
    * Add Creative Commons copyright headers to documentation.
    * Add copyright headers to test code.
    * Add copyright headers to dbus XML that belongs to the NSC.
    * Update copyright headers to GENIVI's standard.
    
    Notable exceptions are:
    * dbus XML that is used to communicate with other services
      (e.g. common/nsm-consumer-dbus.xml) has not had copyright statements
      added, because the Node Startup Controller does not own that interface.

--- nsm-dummy/nsm-consumer-service.h
+++ nsm-dummy/nsm-consumer-service.h
@@ -1,10 +1,20 @@
 /* vi:set et ai sw=2 sts=2 ts=2: */
-/* -
- * Copyright (c) 2012 GENIVI.
+/* SPDX license identifier: MPL-2.0
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Copyright (C) 2012, GENIVI
+ *
+ * This file is part of node-startup-controller.
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License (MPL), v. 2.0.
+ * If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * For further information see http://www.genivi.org/.
+ *
+ * List of changes:
+ * 2015-04-30, Jonathan Maw, List of changes started
+ *
  */
 
 #ifndef __NSM_CONSUMER_SERVICE_H__
